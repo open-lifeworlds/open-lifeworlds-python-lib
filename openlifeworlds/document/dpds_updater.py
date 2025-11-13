0f5cc415eb64111ecf727cce2baba71ff6d00a9b
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-11-13 21:16:58 +0100

    chore: add DPDS updater

---
+++ openlifeworlds/document/dpds_updater.py
@@ -0,0 +1,73 @@
+import os
+from dataclasses import asdict
+
+import yaml
+from openlifeworlds.tracking_decorator import TrackingDecorator
+
+from openlifeworlds.config.dpds_loader import (
+    Info,
+    Owner,
+    InterfaceComponents,
+    OutputPort,
+    InputPort,
+)
+
+
+class IndentDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentDumper, self).increase_indent(flow, False)
+
+
+@TrackingDecorator.track_time
+def update_dpds(data_product_manifest, dpds, config_path):
+    dpds_path = os.path.join(config_path, "dpds.yml")
+
+    if dpds.info is None:
+        dpds.info = Info()
+    if dpds.info.owner is None:
+        dpds.info.owner = Owner()
+
+    dpds.info.id = data_product_manifest.id
+    dpds.info.fullyQualifiedName = (
+        f"urn:dpds:opendataproduct:dataproducts:{data_product_manifest.id}:1.0"
+    )
+    dpds.info.name = data_product_manifest.metadata.name
+    dpds.info.version = "1.0"
+    dpds.info.owner.id = "Open Data product"
+    dpds.info.owner.name = "Open Data product"
+    dpds.info.displayName = data_product_manifest.metadata.name
+    dpds.info.description = data_product_manifest.metadata.description
+
+    if dpds.interfaceComponents is None:
+        dpds.interfaceComponents = InterfaceComponents()
+
+    dpds.interfaceComponents.outputPorts = []
+    dpds.interfaceComponents.inputPorts = []
+
+    for port in data_product_manifest.output_ports:
+        output_port = OutputPort()
+        output_port.id = port.id
+        output_port.fullyQualifiedName = port.id
+        output_port.description = port.metadata.description
+
+        dpds.interfaceComponents.outputPorts.append(output_port)
+
+    for port in data_product_manifest.input_ports:
+        input_port = InputPort()
+        input_port.id = port.id
+        input_port.fullyQualifiedName = port.id
+        input_port.description = port.metadata.description
+
+        dpds.interfaceComponents.inputPorts.append(input_port)
+
+    with open(dpds_path, "w") as file:
+        yaml.dump(
+            asdict(dpds),
+            file,
+            sort_keys=False,
+            default_flow_style=False,
+            Dumper=IndentDumper,
+            allow_unicode=True,
+            width=float("inf"),
+            explicit_start=True,
+        )
---
+++ openlifeworlds/document/odps_updater.py
@@ -0,0 +1,59 @@
+import os
+from dataclasses import asdict
+
+import yaml
+from openlifeworlds.config.odps_loader import ODPS, DataHolder, LocalizedInfo
+from openlifeworlds.config.data_product_manifest_loader import DataProductManifest
+from openlifeworlds.config.odps_loader import Product
+from openlifeworlds.tracking_decorator import TrackingDecorator
+
+
+class IndentDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentDumper, self).increase_indent(flow, False)
+
+
+@TrackingDecorator.track_time
+def update_odps(
+    data_product_manifest: DataProductManifest,
+    odps: ODPS,
+    config_path,
+    output_file_formats: list[str],
+):
+    odps_path = os.path.join(config_path, "odps.yml")
+
+    odps.schema = "https://opendataproducts.org/v3.1/schema/odps.yaml"
+    odps.version = "3.1"
+
+    if odps.product is None:
+        odps.product = Product()
+
+    if odps.product.dataHolder is None:
+        odps.product.dataHolder = DataHolder()
+    odps.product.dataHolder.URL = "https://opendataproduct.de"
+
+    if odps.product.en is None:
+        odps.product.en = LocalizedInfo()
+
+    odps.product.en.name = data_product_manifest.metadata.name
+    odps.product.en.productID = data_product_manifest.id
+    odps.product.en.description = data_product_manifest.metadata.description
+    odps.product.en.version = "1.0"
+    odps.product.en.tags = data_product_manifest.tags
+    odps.product.en.type = "source-aligned"
+    odps.product.en.logoURL = "https://raw.githubusercontent.com/open-data-product/open-data-product-berlin-lor-population-source-aligned/refs/heads/main/logo-with-text.png"
+    odps.product.en.OutputFileFormats = (
+        output_file_formats if output_file_formats is not None else []
+    )
+
+    with open(odps_path, "w") as file:
+        yaml.dump(
+            asdict(odps),
+            file,
+            sort_keys=False,
+            default_flow_style=False,
+            Dumper=IndentDumper,
+            allow_unicode=True,
+            width=float("inf"),
+            explicit_start=True,
+        )
