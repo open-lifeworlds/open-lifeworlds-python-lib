c3c6807d0a3dcf01825da7c0a892482b8f02c5ca
Florian Schwanz <florian.schwanz@interoberlin.de>
2026-01-21 17:22:35 +0100

    chore: update point generator

--- openlifeworlds/transform/data_point_generator.py
+++ openlifeworlds/transform/data_point_generator.py
@@ -10,28 +10,38 @@ from openlifeworlds.tracking_decorator import TrackingDecorator
 
 @TrackingDecorator.track_time
 def generate_points(
-    source_path, results_path, grid_spacing_meters=1_000, clean=False, quiet=False
+    results_path,
+    query,
+    geojson_feature,
+    grid_spacing_meters=1_000,
+    clean=False,
+    quiet=False,
 ):
     """
     Generates points in a given grid
-    :param source_path: source path
     :param results_path: results path
+    :param query: query
+    :param geojson_feature: geojson_feature
     :param grid_spacing_meters: grid spacing in meters
     :param clean: clean
     :param quiet: quiet
     :return:
     """
 
-    city_geojson_path = os.path.join(
-        source_path, "berlin-lor-city", "berlin-lor-city.geojson"
+    # Define area prefix
+    area_prefix = (
+        "-".join(list(reversed(query.split(",")))[1:]).lower().replace(" ", "")
     )
+
+    # Define file paths
     points_geojson_path = os.path.join(
-        results_path, f"berlin-points", f"points-{grid_spacing_meters}.geojson"
+        results_path,
+        f"{area_prefix}-points",
+        f"{area_prefix}-points-{grid_spacing_meters}.geojson",
     )
 
     if clean or not os.path.exists(points_geojson_path):
-        city_geojson = load_geojson_file(city_geojson_path)
-        points = generate_points_in_grid(city_geojson, grid_spacing_meters)
+        points = generate_points_in_grid(geojson_feature, grid_spacing_meters)
         points_geojson = generate_geojson(points)
         write_geojson_file(points_geojson_path, points_geojson, clean, quiet)
     else:
@@ -45,8 +55,9 @@ def load_geojson_file(geojson_template_file_path):
         return json.load(geojson_file, strict=False)
 
 
-def generate_points_in_grid(geojson, grid_spacing_meters):
-    xmin, ymin, xmax, ymax = geojson["features"][0]["properties"]["bounding_box"]
+def generate_points_in_grid(geojson_feature, grid_spacing_meters):
+    xmin, ymin, xmax, ymax = build_bounding_box_with_padding(geojson_feature)
+
     factor_degree_to_meters = (
         111000  # 1 degree of latitude is approximately 111000 meters
     )
@@ -68,9 +79,29 @@ def generate_points_in_grid(geojson, grid_spacing_meters):
     points = np.vstack([grid_longitudes.ravel(), grid_latitudes.ravel()]).T
 
     # Filter points outside the features in the geojson
-    points = [point for point in points if is_point_inside_any_feature(geojson, point)]
+    points = [
+        point for point in points if is_point_inside_feature(geojson_feature, point)
+    ]
 
     return points
+
+
+def build_bounding_box_with_padding(geojson_feature):
+    min_x, min_y, max_x, max_y = geojson_feature["properties"]["bounding_box"]
+
+    padding_horizontal = abs(max_x - min_x) * 0.1
+    padding_vertical = abs(max_y - min_y) * 0.1
+
+    return [
+        min_x - padding_horizontal,
+        min_y - padding_vertical,
+        max_x + padding_horizontal,
+        max_y + padding_vertical,
+    ]
+
+
+def is_point_inside_feature(geojson_feature, point):
+    return shape(geojson_feature["geometry"]).contains(Point(point[0], point[1]))
 
 
 def generate_geojson(points):
@@ -91,22 +122,9 @@ def generate_geojson(points):
     }
 
 
-#
-# Helpers
-#
-
-
 def read_geojson_file(file_path):
     with open(file=file_path, mode="r", encoding="utf-8") as geojson_file:
         return json.load(geojson_file, strict=False)
-
-
-def is_point_inside_any_feature(geojson, point):
-    for feature in geojson["features"]:
-        geometry = shape(feature["geometry"])
-        if geometry.contains(Point(point[0], point[1])):
-            return True
-    return False
 
 
 def write_geojson_file(target_file_path, geojson, clean, quiet):
